17de49675f896fbbeef4b00f3e7626e7f819edf1
Mihoci <mihoci.patrik166@gmail.com>
2019-03-12 14:42:17 +0100

    wrapped with try catch

--- src/index.js
+++ src/index.js
@@ -29,54 +29,59 @@ app.listen(PORT, () => {
 
 
 const  db = async () => {
-
-    //db connection setup
-    const sequelize = new Sequelize('weblica', 'root', 'password', {
-        host: 'localhost',
-        dialect: 'mysql'
-    })
-
     try{
-        await sequelize.query('alter table feedback drop foreign key feedback_ibfk_1;')
-        await sequelize.query('alter table feedback drop foreign key feedback_ibfk_2;')
-        await sequelize.query('DROP TABLE feedback, users, presentations')
+
+        //db connection setup
+        const sequelize = new Sequelize('weblica', 'root', 'password', {
+            host: 'localhost',
+            dialect: 'mysql'
+        })
+
+        try{
+            await sequelize.query('alter table feedback drop foreign key feedback_ibfk_1;')
+            await sequelize.query('alter table feedback drop foreign key feedback_ibfk_2;')
+            await sequelize.query('DROP TABLE feedback, users, presentations')
+
+        }catch(e){
+            console.log('failed to alter tables(they probably dont exist)')
+        }
+
+        //drop tables and create new ones
+        const user = User(sequelize, Sequelize)
+        await user.sync({force: true})
+
+        const presentation = Presentation(sequelize, Sequelize)
+        await presentation.sync({force: true})
+
+        const feedback = Feedback(sequelize, Sequelize)
+        await feedback.sync({force: true})
+
+        //fill tables
+        await user.create({
+            first_name: 'ime',
+            last_name: 'prezime',
+            email: 'mail@mail.com',
+            phone: '777-7777-7777',
+            annonymous: 1
+        })
+
+        await presentation.create({
+            name: 'prezentacija 1',
+            speaker: 'Roberto Kedmenec',
+            duration: 30,
+            start: Date.now()
+        })
+
+        await feedback.create({
+            presentation_id: 1,
+            user_id: 1,
+            rating: 10,
+            comment: 'Sve je dobro'
+        })
 
     }catch(e){
-        console.log('failed to alter tables(they probably dont exist)')
+        console.log(e)
     }
-
-    //drop table users and create new one
-    const user = User(sequelize, Sequelize)
-    await user.sync({force: true})
-
-    const presentation = Presentation(sequelize, Sequelize)
-    await presentation.sync({force: true})
-
-    const feedback = Feedback(sequelize, Sequelize)
-    await feedback.sync({force: true})
-
-
-    await user.create({
-        first_name: 'ime',
-        last_name: 'prezime',
-        email: 'mail@mail.com',
-        phone: '777-7777-7777',
-        annonymous: 1
-    })
-
-    await presentation.create({
-        name: 'prezentacija 1',
-        speaker: 'Roberto Kedmenec',
-        duration: 30,
-        start: Date.now()
-    })
-
-    await feedback.create({
-        presentation_id: 1,
-        user_id: 1,
-        rating: 10,
-        comment: 'Sve je dobro'
-    })
 
 }
 

c1c2f4fa82da1a704fc8e0bc3915bdc6184b6513
Mihoci <mihoci.patrik166@gmail.com>
2019-03-13 08:35:49 +0100

    moved db operations into separate file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import Sequelize from 'sequelize'
 
-import User from './models/users'
-import Presentation from './models/presentations'
-import Feedback from './models/feedback'
+import Db from './db'
 
 
 // Set up the express app
@@ -16,7 +14,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// get all todos
+// get hello
 app.get('/', (req, res) => {
     res.status(200).send(
         "hello"
@@ -29,69 +27,5 @@ app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 });
 
-
-const db = async () => {
-
-    try {
-
-        //db connection setup
-        const sequelize = new Sequelize('weblica', 'root', 'password', {
-            host: 'localhost',
-            dialect: 'mysql'
-        })
-
-        try {
-            await sequelize.query('alter table feedback drop foreign key feedback_ibfk_1;')
-            await sequelize.query('alter table feedback drop foreign key feedback_ibfk_2;')
-            await sequelize.query('DROP TABLE feedback, users, presentations')
-
-        } catch (e) {
-            console.log('failed to alter tables(they probably dont exist)')
-        }
-
-        //drop tables and create new ones
-        const user = User(sequelize, Sequelize)
-        await user.sync({
-            force: true
-        })
-
-        const presentation = Presentation(sequelize, Sequelize)
-        await presentation.sync({
-            force: true
-        })
-
-        const feedback = Feedback(sequelize, Sequelize)
-        await feedback.sync({
-            force: true
-        })
-
-        //fill tables
-        await user.create({
-            first_name: 'ime',
-            last_name: 'prezime',
-            email: 'mail@mail.com',
-            phone: '777-7777-7777',
-            annonymous: 1
-        })
-
-        await presentation.create({
-            name: 'prezentacija 1',
-            speaker: 'Roberto Kedmenec',
-            duration: 30,
-            start: Date.now()
-        })
-
-        await feedback.create({
-            presentation_id: 1,
-            user_id: 1,
-            rating: 10,
-            comment: 'Sve je dobro'
-        })
-
-    } catch (e) {
-        console.log(e)
-    }
-
-}
-
-db();
+//create table and fill it
+Db(Sequelize);
